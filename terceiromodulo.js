84ca7c11ad1f2aaa8dc7b4eb65e5bb0156e39a55
Entrapta-v <beatriz98t@gmail.com>
2020-07-17 19:51:25 -0300

    Terceiro exercicio

--- terceiromodulo.js
+++ terceiromodulo.js
@@ -8,11 +8,22 @@ idade o resultado deve cair no .then, caso contrário, no .catch
 var input = document.createElement('input');
 var btn = document.createElement('button')
 var textbtn = document.createTextNode('Veja a resposta')
+var li = document.createElement('li')
+var ul =  document.querySelector('ul')
 var body = document.querySelector('body')
+var img = document.createElement('img')
 
+ul.appendChild(li)
 btn.appendChild(textbtn)
 body.appendChild(input)
+body.appendChild(img)
 body.appendChild(btn)
+
+var lista = [ 
+    // login = 'Teste',
+    avatar=  ''
+]
+
 
 function checaIdade(idade) {
  // Retornar uma promise
@@ -23,8 +34,6 @@ function checaIdade(idade) {
             reject('não é maior')
         }
     })
-
-
 }
 
 
@@ -46,7 +55,23 @@ function checaIdade(idade) {
 Crie uma tela com um <input> que deve receber o nome de um usuário no Github. Após digitar o
 nome do usuário e clicar no botão buscar a aplicação deve buscar pela API do Github (conforme
 URL abaixo) os dados de repositórios do usuário e mostrá-los em tela:
-URL de exemplo: https://api.github.com/users/diego3g/repos
+URL de exemplo: https://api.github.com/users/betr-v/repos
 Basta alterar "diego3g" pelo nome do usuário
 */
 
+btn.onclick = function(){
+axios.get(`https://api.github.com/users/${input.value}`)
+   .then(function(response){
+    // lista.push(response.data.login);
+    img.src = response.data.avatar_url;
+    lista.push(response.data.avatar_url);
+       console.log(response)
+   })
+   .catch(function(error){
+       console.warn(error)
+   })
+   
+ 
+   li.innerHTML = textContent = lista
+
+}
