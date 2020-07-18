259ef100bedd6e85e05bcdb29ab71be8354250da
Entrapta-v <beatriz98t@gmail.com>
2020-07-18 17:53:51 -0300

    Criando uma lista utilizando api do git

--- JavascriptES6/src/main.js
+++ JavascriptES6/src/main.js
@@ -1,16 +1,63 @@
-const minhaPromise = () => new Promise((resolve, reject) =>{
-    setTimeout(() =>{ resolve('OK')},2000);
-});
+import api from './api';
+class App {
+    constructor (){
+        this.repositories = [];
 
+        this.formEl = document.getElementById('repo-form');
+        this.inputEl = document.getElementById('input-repository');
+        this.listEl = document.getElementById('repo-list')
+        this.registerHandlers();
+    }
 
-// async function executaPromise(){
-//     console.log(await minhaPromise());
-// }
+    registerHandlers(){
+        this.formEl.onsubmit = event => this.addRepository(event); 
+    }
 
-// executaPromise();
+  async  addRepository(event){
+        event.preventDefault();
+        const repoInput = this.inputEl.value;
+     
+        if(repoInput.length === 0){
+            return;
+        }
+        console.log(repoInput);
 
-const executaPromise = async () =>{
-    console.log(await minhaPromise());
+        const response = await api.get(`/repos/${repoInput}`);
+
+        console.log(response)
+        this.repositories.push({
+            name: 'rocketset.com.br',
+            descrition: 'Tire a sua ideia do papel e dê vida à sua startup',
+            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
+            html_url: 'http://github.com/rocketseat/rocketseat.com.br'
+        });
+        this.render();
+    }
+    render(){
+        this.listEl.innerHTML = '';
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.descriptionEl))
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank')
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        })
+    }
 }
 
-executaPromise();
+new App();
